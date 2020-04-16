5a231b99a848105d83723e8bab81ab46c1f356e0
Zyloci <pedrodalmazovaz@gmail.com>
2020-04-16 11:24:37 -0300

    Commit1

---
+++ src/main/java/Group9/ContaMagica.java
@@ -0,0 +1,15 @@
+package Group9;
+
+public class ContaMagica {
+    public static final int SILVER = 0;
+    public static final int GOLD = 1;
+    public static final int PLATINUM = 2;
+    
+    //double getSaldo();
+	
+    //int getStatus();
+	
+    //void deposito(int valor) throws INVALID_OPER_EXCEPTION;
+
+    //void retirada(int valor) throws INVALID_OPER_EXCEPTION;
+}
