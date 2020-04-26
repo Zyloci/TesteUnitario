853fe9422bc51326c712da72f1285bcf2c862212
Thales Santin <kazztt@hotmail.com>
2020-04-25 22:10:54 -0300

    ContaMagicaTest Debug

--- src/main/java/Group9/ContaMagicaTest.java
+++ src/main/java/Group9/ContaMagicaTest.java
@@ -1,9 +1,22 @@
 package Group9;
 
-import junit.framework.*;
+import static org.junit.jupiter.api.Assertions;
+import static org.junit.jupiter.api.Test;
+
 
 public class ContaMagicaTest extends ContaMagica {
+    private ContaMagica CM; 
 
+    @BeforeEach
+    public void initialize() {
+        this.CM = new ContaMagica();
+    }
 
+    @Test
+    public void getSaldo() {
+        ContaMagica CM = new ContaMagica();
+        double saldo = CM.getSaldo();
+        assertTrue(saldo==0);
+    }
 
 }
